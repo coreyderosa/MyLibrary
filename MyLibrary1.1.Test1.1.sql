be1f3221e34bd93b72e5c16e325e9138522d2d1d
coreyderosa <coreyderosa@gmail.com>
2016-08-02 20:02:42 -0600

    Test file
    Updated test file to successfully answer query #1

--- MyLibrary1.1.Test1.1.sql
+++ MyLibrary1.1.Test1.1.sql
@@ -1,7 +1,9 @@
-SELECT B.Title, BC.No_Of_Copies, LB.BranchName
-FROM tblBook AS B 
-INNER JOIN tblBook_Copies AS BC
-ON B.BookID= BC.BookID
-INNER JOIN tblLibrary_Branch AS LB
-ON LB.BranchName = 'Sharpstown'
-WHERE B.Title = 'The Lost Tribe'
+USE dbMyLibrary
+GO
+
+SELECT *
+FROM tblLibrary_Branch AS LB, tblBook AS B, tblBook_Copies AS BC
+WHERE LB.BranchID = BC.BranchID
+	AND B.Title = 'The Lost Tribe'
+	AND LB.BranchName = 'Sharpstown'
+	AND BC.BookID = '8'
