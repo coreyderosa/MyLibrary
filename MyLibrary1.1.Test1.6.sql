0df98cdf419c90a3ff97d029d7e421cde1d83fcb
coreyderosa <coreyderosa@gmail.com>
2016-08-04 20:24:25 -0600

    Initial Commit #6
    Retrieve the names, addresses, and number of books checked out for all
    borrowers who have more than five books checked out.

---
+++ MyLibrary1.1.Test1.6.sql
@@ -0,0 +1,15 @@
+--Retrieve the names, addresses, and number of books checked out for all borrowers who have more
+--than five books checked out.
+USE dbMyLibrary
+GO
+
+DECLARE @dueDate DATE
+SET @dueDate = GETDATE()
+
+SELECT COUNT(BL.CardNO) AS NumberOfBooksCheckedOut, BR.BorrowerName, BR.BorrowerAddress
+FROM tblBook_Loans AS BL
+INNER JOIN tblBorrower as BR
+ON BL.CardNO = BR.CardNO
+WHERE BL.DueDate > @dueDate
+GROUP BY BR.BorrowerName, BR.BorrowerAddress
+HAVING COUNT(BL.BookID) >= 5
